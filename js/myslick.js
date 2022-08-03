5fafaae3b25776e80becea5843f84c256dfd59ea
vladyslavketov <106628608+vladyslavketov@users.noreply.github.com>
2022-08-04 01:53:59 +0300

    day 6

--- js/myslick.js
+++ js/myslick.js
@@ -1,12 +1,37 @@
 $(document).ready(function(){
-  $('.slider').slick({
-    dots: true,
+  $('.clients__slider').slick({
+    arrows: false,
+    dots: false,
     slidesToShow: 3,
+    speed: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    waitForAnimate: true,
+  });
+  $('.team__slider').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 1,
     speed: 1000,
     // autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed:5000,
     waitForAnimate: false,
-    centerMode: false,
-    variableWidth: false,
+    wariableWidth: true,
+    responsive:[
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        }
+      },{
+        breakpoint: 1200,
+        settings: {
+          arrows: true,
+          slidesToShow: 3,
+          // centerMode: true,
+        }
+      }
+    ],
+    mobileFirst: true,
   });
 });
