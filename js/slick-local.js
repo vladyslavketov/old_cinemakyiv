db713a85b05a60475febf6b42fae6af1ef00a7a4
vladyslavketov <106628608+vladyslavketov@users.noreply.github.com>
2022-08-20 05:01:03 +0300

    new idea | day 1

--- js/slick-local.js
+++ js/slick-local.js
@@ -1,34 +1,43 @@
 $(document).ready(function(){
-  $('.clients__slider').slick({
+  $('.partners__slider').slick({
     arrows: false,
     dots: false,
-    slidesToShow: 3,
+    slidesToShow: 2,
     speed: 1,
     autoplay: true,
     autoplaySpeed: 3000,
     waitForAnimate: true,
+    responsive:[
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 3,
+        }
+      }
+    ],
+    mobileFirst: true,
   });
   $('.team__slider').slick({
     arrows: false,
     dots: true,
     slidesToShow: 1,
     speed: 1000,
-    autoplay: true,
+    // autoplay: true,
     autoplaySpeed:5000,
     waitForAnimate: false,
     wariableWidth: true,
     responsive:[
       {
-        breakpoint: 768,
+        breakpoint: 767,
         settings: {
           slidesToShow: 2,
         }
       },{
-        breakpoint: 1200,
+        breakpoint: 1199,
         settings: {
           arrows: true,
           slidesToShow: 3,
-          // centerMode: true,
+          centerMode: true,
         }
       }
     ],
